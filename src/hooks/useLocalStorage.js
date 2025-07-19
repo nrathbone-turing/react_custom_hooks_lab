cd08419d299a3806e589fbbe004a2d04e3a42822
Nick Rathbone <nrathbone@joinhandshake.com>
2025-07-19 14:37:00 -0600

    Add psuedocode to the custom hook file

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,4 +1,13 @@
 import { useEffect, useState } from "react";
 
 export function useLocalStorage(key, initialValue = null) {
+
+  // 1. Get item from localStorage
+
+  // 2. If found, parse and use as initial state; otherwise, use initialValue
+  
+  // 3. Return [value, setValue]
+
+  // 4. useEffect to update localStorage on value/key change
+
 }
